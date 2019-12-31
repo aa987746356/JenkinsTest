15862415a2d7b72589c38c3bf5864b24fa1ac526
mengpeng <mengpeng@haylion.cn>
2019-12-31 16:39:36 +0800

    change

--- src/main/java/com/meng/demo/controller/Hello.java
+++ src/main/java/com/meng/demo/controller/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
 
     @GetMapping("hello")
     public String hello(){
-        return "hello!? what? change OK ???2323";
+        return "hello!? what? change OK ?";
     }
 
 }
